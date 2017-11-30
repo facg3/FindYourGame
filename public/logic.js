a2303d90765c95c5783ad74f6764ebbf746c6ec0
walaamedhat <nicedree@gmail.com>
2017-11-30 09:59:10 +0200

    edit data list

--- public/logic.js
+++ public/logic.js
@@ -2,17 +2,22 @@
 function gamefinder(){
   url = "/getgame";
   var name= document.getElementById('gamename').value;
+  var datalist = document.getElementById('gameslist');
+  var childArray = datalist.children;
+  var cL = childArray.length;
+        while(cL > 0) {
+            cL--;
+            datalist.removeChild(childArray[cL]);
+        }
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function () {
     if (xhr.status === 200) {
       var response = (xhr.responseText);
       response=JSON.parse(response);
-      var datalist = document.getElementById('gameslist');
     for (var i = 0; i < response.length; i++) {
         var option =document.createElement('option');
         option.setAttribute("value", response[i]);
         datalist.appendChild(option);
-
     }
 
 
